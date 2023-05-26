23c6b62ad66ffa1eae5ef2248231c755edb5cc5f
lumusitech <lumusitech@gmail.com>
2023-05-25 23:02:29 -0300

    feat: add routes

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route, Navigate } from "react-router-dom"
+
+import { CalendarPage } from "../calendar"
+import { LoginPage } from "../auth"
+
+export const AppRouter = () => {
+  const authStatus = 'not-authenticated'
+
+  return (
+    <Routes>
+      {
+        (authStatus === 'not-authenticated')
+          ? <Route path="/auth/*" element={<LoginPage />} />
+          : <Route path="/*" element={<CalendarPage />} />
+      }
+
+      <Route path="/*" element={<Navigate to='/auth/login' />} />
+    </Routes>
+  )
+}
