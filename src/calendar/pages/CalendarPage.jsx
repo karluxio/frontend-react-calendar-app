d4dfb7c39005c0cb08dd10d73e0253059f8a7af0
lumusitech <lumusitech@gmail.com>
2023-06-05 13:09:05 -0300

    refactor: add helpers to spanish translate and style config

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,21 +1,11 @@
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
-import enUS from 'date-fns/locale/en-US'
-import { addHours, format, parse, startOfWeek, getDay } from 'date-fns'
+import { Calendar } from 'react-big-calendar'
+import { addHours } from 'date-fns'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 
 import { Navbar } from "../"
+import { localizer, getMessages } from '../../helpers'
 
-const locales = {
-  'en-US': enUS,
-}
 
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
-})
 
 const events = [
   {
@@ -32,17 +22,34 @@ const events = [
 ]
 
 export const CalendarPage = () => {
+
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    console.log({ event, start, end, isSelected });
+
+    const style = {
+      backgroundColor: '#347CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white'
+    }
+
+    return { style }
+  }
+
   return (
     <>
       <Navbar />
 
 
       <Calendar
+        culture='es'
         localizer={localizer}
         events={events}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 'calc(100vh - 80px)' }}
+        messages={getMessages()}
+        eventPropGetter={eventStyleGetter}
       />
 
     </>
