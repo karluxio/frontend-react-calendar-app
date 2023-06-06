a5897ddbef3868cccc620204a8284bfb18e479ce
lumusitech <lumusitech@gmail.com>
2023-06-06 20:30:30 -0300

    feat: add initial store using redux-toolkit

---
+++ src/store/calendar/calendarSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const calendarSlice = createSlice({
+  name: 'calendar',
+  initialState: {
+    // counter: 10
+  },
+  reducers: {
+    // increment: (state) => {
+    //   state.counter += 1
+    // },
+    // decrement: (state) => {
+    //   state.counter -= 1
+    // },
+    // incrementBy: (state, action) => {
+    //   state.counter += action.payload
+    // }
+  },
+})
+// export const { increment, decrement, incrementBy } = calendarSlice.actions
