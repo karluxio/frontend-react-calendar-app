6d70b9f7949884c54555ba513d751aea40b6e24c
karlux <karlux.sistemas@gmail.com>
2023-07-05 16:34:03 -0300

    feat: add form handling on login page

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,16 +1,61 @@
+import { useForm } from '../../hooks';
 import './login.css';
 
+const loginFormsFields = {
+  loginEmail: '',
+  loginPassword: ''
+}
+
+const registerFormsFields = {
+  registerName: '',
+  registerEmail: '',
+  registerPassword: '',
+  registerPassword2: ''
+}
+
 export const LoginPage = () => {
+
+  const {
+    loginEmail,
+    loginPassword,
+    onInputChange: onLoginInputChange,
+    onResetForm: onLoginResetForm
+  } = useForm(loginFormsFields)
+
+  const {
+    registerName,
+    registerEmail,
+    registerPassword,
+    registerPassword2,
+    onInputChange: onRegisterInputChange,
+    onResetForm: onRegisterResetForm
+  } = useForm(registerFormsFields)
+
+  const loginSubmit = (event) => {
+    event.preventDefault()
+    console.log({ loginEmail, loginPassword });
+    onLoginResetForm()
+  }
+
+  const registerSubmit = (event) => {
+    event.preventDefault()
+    console.log({ registerEmail, registerPassword, registerPassword2, registerName });
+    onRegisterResetForm()
+  }
+
   return (
     <div className="container login-container">
       <div className="row">
         <div className="col-md-6 login-form-1">
           <h3>Sign In</h3>
-          <form>
+          <form onSubmit={loginSubmit}>
             <div className="form-group mb-2">
               <input
                 type="text"
                 className="form-control"
+                name='loginEmail'
+                value={loginEmail}
+                onChange={onLoginInputChange}
                 placeholder="Email"
               />
             </div>
@@ -19,6 +64,9 @@ export const LoginPage = () => {
                 type="password"
                 className="form-control"
                 placeholder="Password"
+                name='loginPassword'
+                onChange={onLoginInputChange}
+                value={loginPassword}
               />
             </div>
             <div className="form-group mb-2">
@@ -33,12 +81,15 @@ export const LoginPage = () => {
 
         <div className="col-md-6 login-form-2">
           <h3>Register</h3>
-          <form>
+          <form onSubmit={registerSubmit}>
             <div className="form-group mb-2">
               <input
                 type="text"
                 className="form-control"
                 placeholder="Name"
+                name='registerName'
+                value={registerName}
+                onChange={onRegisterInputChange}
               />
             </div>
             <div className="form-group mb-2">
@@ -46,6 +97,9 @@ export const LoginPage = () => {
                 type="email"
                 className="form-control"
                 placeholder="Email"
+                name='registerEmail'
+                value={registerEmail}
+                onChange={onRegisterInputChange}
               />
             </div>
             <div className="form-group mb-2">
@@ -53,6 +107,9 @@ export const LoginPage = () => {
                 type="password"
                 className="form-control"
                 placeholder="Password"
+                name='registerPassword'
+                value={registerPassword}
+                onChange={onRegisterInputChange}
               />
             </div>
 
@@ -61,6 +118,9 @@ export const LoginPage = () => {
                 type="password"
                 className="form-control"
                 placeholder="Repeat password"
+                name='registerPassword2'
+                value={registerPassword2}
+                onChange={onRegisterInputChange}
               />
             </div>
 
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,7 +4,7 @@ import { CalendarPage } from "../calendar"
 import { LoginPage } from "../auth"
 
 export const AppRouter = () => {
-  const authStatus = 'authenticated'
+  const authStatus = 'not-authenticated'
 
   return (
     <Routes>
