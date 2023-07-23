71bf5a4db3ed46b59ee0d36f4eabe6662d3a47e9
karlux <karlux.sistemas@gmail.com>
2023-07-23 19:28:58 -0300

    feat: change to hashRouter

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -3,14 +3,17 @@ import { AppRouter } from "./router"
 import { Provider } from 'react-redux'
 
 import { store } from "./store"
+import { HashRouter } from "react-router-dom"
 
 export const CalendarApp = () => {
   return (
     <>
       <Provider store={store}>
-        <BrowserRouter>
+        {/* <BrowserRouter> */}
+        <HashRouter>
           <AppRouter />
-        </BrowserRouter>
+        </HashRouter>
+        {/* </BrowserRouter> */}
       </Provider>
     </>
   )
